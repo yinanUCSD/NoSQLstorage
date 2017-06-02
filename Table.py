a5773671e4f94ced7987fb1a2e2699d1c2a1ad4d
liyinan <liyinan@didichuxing.com>
2017-06-02 16:13:24 -0700

    prototype

--- Table.py
+++ Table.py
@@ -1,12 +1,12 @@
 from Column import Column
 class Table:
     def __init__(self, cols=None, tablePath=None):
-        if cols != None:
+        if cols != None:      #build new table
             for col in cols:
                 self.cols[col] = Column(colname=col)
-        if tablePath != None:
+        if tablePath != None:  #load table from file
             self.load(tablePath)
-    def load(self, tablePath):  #load table from file
+    def load(self, tablePath):
         pass
     def insert(self, tuple):
         pass
