d6d31b86f6f8c86c6891b69e5ca5890c1e3b5ee0
zeromike <785547425@qq.com>
2017-06-05 21:22:21 -0700

    test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from Table import Table
 
 t1 = Table()
-t1.newtable(cols=['ID', 'IDE', 'OS'], tablepath='./table1/', tablename='t1')
+t1.newtable(cols=['ID', 'gender', 'OS', 'salary'], tablepath='./table1/', tablename='t1')
 t2 = Table()
 t2.loadFrom(tablepath='./table2/')
 
@@ -10,7 +10,8 @@ t1.set({'ID': 'alice', 'gender': 'female', 'OS': 'macOS','salary':'2000' })
 t1.set({'ID': 'charlie', 'gender': 'male', 'OS': 'windows', 'salary':'3000'})
 
 
-print t1.select(cols=["ID", "OS"], where={"gender": "male"})
+#print t1.select(cols=["ID", "OS"], where={"gender": "male"})
+print t1.select(cols=["OS", "salary"], where={"gender": "male"})
 
 print t1.join(t2)
 print t1.sum('salary')
