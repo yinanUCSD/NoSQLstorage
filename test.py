051e204ee14e615c7c87ecacc27732b3e0606b14
zeromike <785547425@qq.com>
2017-06-06 14:06:52 -0700

    test

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from Table import Table
 t1 = Table()
 t1.newtable(cols=['ID', 'gender', 'OS', 'salary'], tablepath='./table1/', tablename='t1')
 t2 = Table()
-t2.loadFrom(tablepath='./table2/')
+t2.newtable(cols=['ID', 'gender', 'OS', 'salary'], tablepath='./table2/', tablename='t2')
 
 t1.set({'ID': 'bob', 'gender': 'male', 'OS': 'Linux', 'salary':'1000'})
 t1.set({'ID': 'alice', 'gender': 'female', 'OS': 'macOS','salary':'2000' })
 t1.set({'ID': 'charlie', 'gender': 'male', 'OS': 'windows', 'salary':'3000'})
-
+t2.set({'ID': 'bob', 'gender': 'female', 'OS': 'windows', 'salary':'4000'})
 
 #print t1.select(cols=["ID", "OS"], where={"gender": "male"})
 print t1.select(cols=["OS", "salary"], where={"gender": "male"})
